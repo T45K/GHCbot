0a374aef61191eacb0256dc85464225ba1e405cd
tsk-nkgw <tasktas9@gmail.com>
2019-06-08 00:40:03 +0900

    fix

--- src/main/kotlin/t45k/ghcbonk/App.kt
+++ src/main/kotlin/t45k/ghcbonk/App.kt
@@ -14,37 +14,37 @@ class App {
     companion object {
         const val TIME_OF_ONE_DAY: Long = 1000 * 60 * 60 * 24
     }
+}
 
-    fun main(args: Array<String>) {
-        val properties = Properties()
-        properties.load(Files.newBufferedReader(Paths.get(args[0])))
+fun main(args: Array<String>) {
+    val properties = Properties()
+    properties.load(Files.newBufferedReader(Paths.get(args[0])))
 
-        val timer = Timer()
-        val timerTask = object : TimerTask() {
-            override fun run() {
-                val userName = properties.getProperty("userName")
-                val user = GitHubUser(userName)
-                val contributionData = user.fetchContributionData()
-                val tweeter = Tweeter(properties)
-                tweeter.tweet(contributionData)
-            }
+    val timer = Timer()
+    val timerTask = object : TimerTask() {
+        override fun run() {
+            val userName = properties.getProperty("userName")
+            val user = GitHubUser(userName)
+            val contributionData = user.fetchContributionData()
+            val tweeter = Tweeter(properties)
+            tweeter.tweet(contributionData)
         }
+    }
 
-        val date = Date()
-        val calendar = Calendar.getInstance()
-        calendar.time = date
-        val simpleDateFormat = SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
-        val format = simpleDateFormat.format(calendar.time)
+    val date = Date()
+    val calendar = Calendar.getInstance()
+    calendar.time = date
+    val simpleDateFormat = SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
+    val format = simpleDateFormat.format(calendar.time)
 
-        if (format.endsWith("00:00:00")) {
-            timer.schedule(timerTask, TIME_OF_ONE_DAY)
-        } else {
-            calendar.add(Calendar.DATE, 1)
-            calendar.set(Calendar.HOUR_OF_DAY, 0)
-            calendar.set(Calendar.MINUTE, 0)
-            calendar.set(Calendar.SECOND, 0)
-            timer.schedule(timerTask, calendar.time, TIME_OF_ONE_DAY);
-        }
+    if (format.endsWith("00:00:00")) {
+        timer.schedule(timerTask, App.TIME_OF_ONE_DAY)
+    } else {
+        calendar.add(Calendar.DATE, 1)
+        calendar.set(Calendar.HOUR_OF_DAY, 0)
+        calendar.set(Calendar.MINUTE, 0)
+        calendar.set(Calendar.SECOND, 0)
+        timer.schedule(timerTask, calendar.time, App.TIME_OF_ONE_DAY);
     }
 }
 
