49d9beb9e519d7c8c83fdc08c3ca2775d912b8e3
tsk-nkgw <tasktas9@gmail.com>
2019-12-24 00:56:46 +0900

    fix

--- src/main/kotlin/t45k/ghcbonk/github/Analyzer.kt
+++ src/main/kotlin/t45k/ghcbonk/github/Analyzer.kt
@@ -1,18 +1,13 @@
 package t45k.ghcbonk.github
 
-fun analyzeRawData(rawData: Array<String>, userName: String): ContributionData {
+fun analyzeRawData(rawData: List<String>, userName: String): ContributionData {
     var counter = 0
 
-    rawData.forEach { oneDayActivity: String ->
+    for (oneDayActivity: String in rawData) {
         val elements: List<String> = oneDayActivity.split("//s+")
         val numOfContributionOfTheDay: String = getStringInDoubleQuote(elements[8])
 
-        if (numOfContributionOfTheDay == "0") {
-            counter = 0
-            return@forEach
-        }
-
-        counter++
+        counter = if (numOfContributionOfTheDay == "0") 0 else counter + 1
     }
 
     val lastActivity: List<String> = rawData[rawData.size - 1].split("//s+")
--- src/main/kotlin/t45k/ghcbonk/github/GitHubUser.kt
+++ src/main/kotlin/t45k/ghcbonk/github/GitHubUser.kt
@@ -7,30 +7,37 @@ import java.io.Reader
 import java.net.HttpURLConnection
 import java.net.URL
 import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
+import kotlin.streams.toList
 
 class GitHubUser(private val userName: String) {
     companion object {
         private const val USER_URL_PREFIX = "https://github.com/users/"
-        private const val USER_URL_SUFFIX = "/contributions"
     }
 
     private val userURL: URL
+    private val today: String
 
     init {
-        this.userURL = URL(USER_URL_PREFIX + userName + USER_URL_SUFFIX)
+        this.userURL = URL(USER_URL_PREFIX + userName)
+
+        val date = Date()
+        val calendar: Calendar = Calendar.getInstance()
+        calendar.time = date
+        today = SimpleDateFormat("yyyy-MM-dd", Locale.JAPAN).format(calendar.time)
     }
 
     fun fetchContributionData(): ContributionData {
-
         val connection: HttpURLConnection = setConnection()
-        val rawData: Array<String> = fetchRawData(connection)
+        val rawData: List<String> = fetchHTMLSource(connection)
 
         return analyzeRawData(rawData, userName)
     }
 
     private fun setConnection(): HttpURLConnection {
-        val connection = userURL.openConnection() as HttpURLConnection
+        val connection: HttpURLConnection = userURL.openConnection() as HttpURLConnection
         connection.requestMethod = "GET"
         connection.instanceFollowRedirects = false
         connection.connect()
@@ -38,31 +45,24 @@ class GitHubUser(private val userName: String) {
         return connection
     }
 
-    private fun fetchRawData(connection: HttpURLConnection): Array<String> {
+    private fun fetchHTMLSource(connection: HttpURLConnection): List<String> {
         val status: Int = connection.responseCode
         return if (status == HttpURLConnection.HTTP_OK) {
             readInputStream(connection.inputStream)
         } else {
-            emptyArray()
+            emptyList()
         }
     }
 
-    private fun readInputStream(inputStream: InputStream): Array<String> {
+    private fun readInputStream(inputStream: InputStream): List<String> {
         val inputStreamReader = InputStreamReader(inputStream)
-        val bufferedReader = BufferedReader(inputStreamReader as Reader?)
+        val bufferedReader = BufferedReader(inputStreamReader as Reader)
         return bufferedReader.lines()
                 .filter { isNecessaryInformation(it) }
-                .toArray<String> { length -> arrayOfNulls(length) }
+                .toList()
     }
 
     private fun isNecessaryInformation(line: String): Boolean {
-        return line.contains("<rect") && !line.contains(getDate())
-    }
-
-    private fun getDate(): String {
-        val date = Date()
-        val calendar = Calendar.getInstance()
-        calendar.time = date
-        return SimpleDateFormat("yyyy-MM-dd", Locale.JAPAN).format(calendar.time)
+        return line.contains("<rect") && !line.contains(today)
     }
 }
